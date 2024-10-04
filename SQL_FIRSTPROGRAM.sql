0bef0e87706bf8d776235a96447091b66ae35abf
Riya Ambastha <ambasthariya58@gmail.com>
2024-10-03 23:41:25 -0700

    Add files via upload

---
+++ SQL_FIRSTPROGRAM.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE company;
+
+USE company;
+
+CREATE TABLE employee (
+  id int primary key,
+  name varchar(50),
+  salary INT NOT NULL
+);
+
+SELECT*FROM employee;
+
+INSERT INTO employee 
+(id , name , salary)
+values
+(1, "ADAM" , 20000),
+(2 , "BOB" , 30000),
+(3 , "CRASEY" , 40000);
+
+
+
+
+ 
+ 
+ 
+ 
+
+  
+
+
+
+
+ 
+
+
+
+
+
+ 
+
